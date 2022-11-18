b4fead65b5f1d5e1efb13ee472a06f6ca6c8a3b3
K-156 <limstephanie156@gmail.com>
2022-11-18 12:27:22 +0800

    finish stripe

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -4,7 +4,36 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  const [showLinks, setShowLinks] = useState(false)
+  return (
+  <nav>
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="logo" />
+        <button className="nav-toggle">
+          <FaBars />
+        </button>
+        <div className="links-container show-container">
+          <ul className="links">
+            {links.map((link) => {
+              const {id, url, text} = link;
+              return <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            })}
+          </ul>
+          <ul className="social-icons">
+            {social.map((socialIcon) => {
+                const {id, url, icon} = socialIcon;
+                return <li key={id}>
+                  <a href={url}>{icon}</a>
+                </li>
+              })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>)
 }
 
 export default Navbar
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,35 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({children}) => {
+    const [isSiderbarOpen, setIsSidebarOpen] = useState(true);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+    const openSideBar = () => {
+        setIsSidebarOpen(true);
+    }
+
+    const closeSideBar = () => {
+        setIsSidebarOpen(false);
+    }
+
+    const openSubmenu = () => {
+        setIsSubmenuOpen(true);
+    }
+
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false);
+    }
+
+    return <AppContext.Provider value={{
+        isSubmenuOpen,isSiderbarOpen, openSubmenu, openSideBar, closeSubmenu, closeSideBar
+    }}>
+        {children}
+    </AppContext.Provider>
+} 
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
